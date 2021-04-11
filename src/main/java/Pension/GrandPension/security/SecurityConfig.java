96c852b8c6952197ac41ab35b477a842e7a37288
jieun <jieunjeong9494@gmail.comqjava>
2021-04-12 01:03:35 +0900

    jdbc

--- src/main/java/Pension/GrandPension/security/SecurityConfig.java
+++ src/main/java/Pension/GrandPension/security/SecurityConfig.java
@@ -1,15 +1,22 @@
 package Pension.GrandPension.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 //설정관련
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //db랑 연결해주는거
+    @Autowired
+    DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -24,11 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        //메모리상에 강제로 쓰는거
-        auth.inMemoryAuthentication()
-                .withUser("user1").password("password1").authorities("ROLE_USER")
-                .and().withUser("USER2").password("{noop}password2").authorities("ROLE_USER");
-    }
+       auth.jdbcAuthentication().dataSource(dataSource);
 
-
+         }
 }
